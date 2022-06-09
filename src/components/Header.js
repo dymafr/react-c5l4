11a68daff71221841da215b316c47b6e003be19e
dymafr <erwan1309@hotmail.com>
2022-06-09 13:44:49 +0200

    2 files modified, 1 added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import styles from './Header.module.scss';
+import cookchef from '../assets/images/cookchef.png';
 
 function Header() {
-  return <h1 className="b1">Header</h1>;
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <i class="fa-solid fa-bars mr-15"></i>
+      <div className="flex-fill">
+        <img src={cookchef} alt="logo cookchef" />
+      </div>
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i class="fa-solid fa-basket-shopping mr-5"></i>
+          <span>panier</span>
+        </button>
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
