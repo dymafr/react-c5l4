16311d4d669a0c1dd659ab0a820e6aaa0c2517d2
dymafr <erwan1309@hotmail.com>
2022-06-09 13:53:35 +0200

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import cookchef from 'https://github.com/dymafr/react-c5l4/blob/master/src/assets/images/cookchef.png';
 
 function Header() {
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <i class="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
-        <img src={cookchef} alt="logo cookchef" />
+        <img
+          src="https://github.com/dymafr/react-c5l4/blob/master/src/assets/images/cookchef.png"
+          alt="logo cookchef"
+        />
       </div>
       <ul>
         <button className="mr-5 btn btn-reverse-primary">
