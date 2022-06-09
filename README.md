7aba3faaeabbb719226ce186c945330eaecc9cc4
dymafr <erwan1309@hotmail.com>
2022-06-09 13:34:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tzp3my
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tzp3my)
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <h1 className="b2">Footer</h1>;
+}
+
+export default Footer;
